2cb06b92aa797b47163796347ac6f68060cdeea1
olszi <j.olszowka@op.pl>
2023-01-12 19:09:31 +0100

    V2
    Podstwowy ulamek

--- ulamek.cpp
+++ ulamek.cpp
@@ -3,7 +3,25 @@
 #include <time.h>
 using namespace std;
 
+class ulamek
+{
+    int licznikPV,mianownikPV;
+    public:
+    ulamek(int l , int m){
+        if(m==0){
+            mianownikPV =0;
+            licznikPV = l;
+        }else{
+            mianownikPV = m;
+            licznikPV = l;
+        }   
+    }
+    void Wynik(){
+        cout <<licznikPV<<"/"<<mianownikPV<<endl;
+    }
+};
+
 int main() {
-    cout << "Hello World!";
-    return 0;
+    ulamek A(2,4);
+    A.Wynik();
 }

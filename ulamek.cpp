5e6d5268fa0d5c3035986688ee073a7b2f31607d
olszi <j.olszowka@op.pl>
2023-01-12 19:17:55 +0100

    v3
    Napraw błędów i dodanie większej ilosci komunikatów

--- ulamek.cpp
+++ ulamek.cpp
@@ -9,7 +9,8 @@ class ulamek
     public:
     ulamek(int l , int m){
         if(m==0){
-            mianownikPV =0;
+            mianownikPV =1;
+            cout <<"Wprowadzony ulamek jest nieprawidlowy, nie mozesz dzielic przez 0,zamieniam mianownik na 1"<< endl;
             licznikPV = l;
         }else{
             mianownikPV = m;
@@ -17,11 +18,17 @@ class ulamek
         }   
     }
     void Wynik(){
-        cout <<licznikPV<<"/"<<mianownikPV<<endl;
+        cout <<"Wprowadzony ulamek to "<<licznikPV<<"/"<<mianownikPV<<endl;
+        if(licznikPV/mianownikPV==1){
+             cout <<"Wprowadzony ulamek to inaczej 1" <<endl;
+        }else if(mianownikPV==1){
+            cout <<"Wprowadzony ulamek mozna zapisac w postaci: "<< licznikPV <<endl;
+        }
     }
+
 };
 
 int main() {
-    ulamek A(2,4);
+    ulamek A(2,0);
     A.Wynik();
 }
